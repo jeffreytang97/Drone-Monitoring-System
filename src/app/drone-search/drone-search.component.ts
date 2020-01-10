56f7b0dca097bb2c260311dbeaa95055babe5ad7
Jonathan <jonathan.cournoyer@scigilian.com>
2020-01-09 22:41:38 -0500

    Made Components for each section of the website, as discussed

---
+++ src/app/drone-search/drone-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-drone-search',
+  templateUrl: './drone-search.component.html',
+  styleUrls: ['./drone-search.component.css']
+})
+export class DroneSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/zone-search/zone-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-zone-search',
+  templateUrl: './zone-search.component.html',
+  styleUrls: ['./zone-search.component.css']
+})
+export class ZoneSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
