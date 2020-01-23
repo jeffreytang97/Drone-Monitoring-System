5bab0a67666f7b7e3c6ff137de2a50a45f3053fb
jeffreytang97 <jeffreytang97@gmail.com>
2020-01-22 23:17:18 -0500

    Added google map api to the project

--- src/app/Components/map/map.component.ts
+++ src/app/Components/map/map.component.ts
@@ -1,11 +1,34 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, AfterViewInit, ViewChild, ElementRef} from '@angular/core';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
+
 export class MapComponent implements OnInit {
+
+  @ViewChild('mapContainer', {static: false}) gmap: ElementRef;
+
+  // Create a new map variable that contains the Google Maps API along with latitude and longitude values
+  map: google.maps.Map;
+  lat = 45.505331312;
+  lng = -73.55249779;
+
+  // Create a coordinate variable to use our latitude and longtitude
+  coordinates = new google.maps.LatLng(this.lat, this.lng);
+
+  // Create a mapOptions variable
+  mapOptions: google.maps.MapOptions = {
+        center: this.coordinates,
+        zoom: 8,
+      };
+
+  // Create a marker
+  marker = new google.maps.Marker({
+    position: this.coordinates,
+    map: this.map,
+  });
 
   constructor() {
   }
@@ -13,4 +36,13 @@ export class MapComponent implements OnInit {
   ngOnInit() {
   }
 
+  mapInitializer() {
+    this.map = new google.maps.Map(this.gmap.nativeElement,
+    this.mapOptions);
+    this.marker.setMap(this.map);
+  }
+
+  ngAfterViewInit() {
+    this.mapInitializer();
+  }
 }
