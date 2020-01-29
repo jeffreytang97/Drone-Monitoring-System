ed6d419db289f44b7fa12b8aef56d2187ecce4d4
Jonathan <jonathan.cournoyer@scigilian.com>
2020-01-29 00:07:56 -0500

    Started working on zone creation

--- src/app/Components/zone-creation/zone-creation.module.ts
+++ src/app/Components/zone-creation/zone-creation.module.ts
@@ -1,11 +1,15 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ZoneCreationComponent} from './zone-creation.component';
+import {MatCardModule} from "@angular/material/card";
+import {MatButtonToggleModule} from "@angular/material/button-toggle";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatButtonModule} from "@angular/material/button";
 
 const MaterialComponents = [];
 
 @NgModule({
-  imports: [MaterialComponents],
+  imports: [MaterialComponents, MatCardModule, MatButtonToggleModule, MatFormFieldModule, MatButtonModule],
   exports: [MaterialComponents, ZoneCreationComponent],
   declarations: [ZoneCreationComponent],
 })
