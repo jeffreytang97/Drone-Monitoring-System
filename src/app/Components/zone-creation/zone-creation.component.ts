3bdfc4e94d836f42df099ac0b2cacd43aaedce08
jeffreytang97 <jeffreytang97@gmail.com>
2020-03-19 13:05:49 -0400

    zone creation changes on creating zones

--- src/app/Components/zone-creation/zone-creation.component.ts
+++ src/app/Components/zone-creation/zone-creation.component.ts
@@ -25,6 +25,8 @@ export class ZoneCreationComponent implements OnInit {
           zoom: 8,
         };
 
+
+
     constructor() {
     }
 
@@ -33,10 +35,53 @@ export class ZoneCreationComponent implements OnInit {
 
     ngAfterViewInit() {
         this.mapInitializer();
+        this.createMarkers();
     }
 
     mapInitializer() {
-      this.map = new google.maps.Map(this.zone_map.nativeElement,
-      this.mapOptions);
+      this.map = new google.maps.Map(this.zone_map.nativeElement, this.mapOptions);
+
+      // This is just an example
+      var citymap = {
+            chicago: {
+              center: {lat: 41.878, lng: -87.629},
+              population: 2714856
+            },
+            newyork: {
+              center: {lat: 40.714, lng: -74.005},
+              population: 8405837
+            },
+            losangeles: {
+              center: {lat: 34.052, lng: -118.243},
+              population: 3857799
+            },
+            vancouver: {
+              center: {lat: 49.25, lng: -123.1},
+              population: 603502
+            }
+          };
+      for (var city in citymap) {
+          // Add the circle for this city to the map.
+          var cityCircle = new google.maps.Circle({
+            strokeColor: '#FF0000',
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: '#FF0000',
+            fillOpacity: 0.35,
+            map: this.map,
+            center: citymap[city].center,
+            radius: Math.sqrt(citymap[city].population) * 100
+          });
     }
+  }
+
+  createMarkers(){
+    // When right-click on the map, it will return coordinates
+    google.maps.event.addListener(this.map, "rightclick", function(event) {
+        var lat = event.latLng.lat();
+        var lng = event.latLng.lng();
+        // populate yor box/field with lat, lng
+        alert("Lat=" + lat + "; Lng=" + lng);
+    });
+  }
 }
